629742c862f519c9826191c4afa0ba0d5f8bbb2d
KoTeuKa404 <135438716+KoTeuKa404@users.noreply.github.com>
2026-07-19 15:07:00 +0300

    Add shared UI constants

---
+++ android_src/ironvale/constants.py
@@ -0,0 +1,11 @@
+from typing import Tuple
+
+VW = 1280.0
+VH = 720.0
+
+TOP_BAR_Y = 638.0
+TOP_BAR_H = 82.0
+BOTTOM_PANEL_H = 126.0
+
+RGBA = Tuple[float, float, float, float]
+Point = Tuple[float, float]
